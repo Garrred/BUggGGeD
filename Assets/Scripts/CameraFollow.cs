b12e1f02e25c1f7ccd3b5accea28632218aac093
Garrred <fchen222@wisc.edu>
2021-11-02 22:46:02 -0500

    Added background, smooth camera follow and clamped positions.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float followSpeed = 10;
+
+    public float maxX;
+    public float minX;
+    public float maxY;
+    public float minY;
+    public Vector2 offset;
+
+    void LateUpdate()
+    {
+        if (playerTransform != null)
+        {
+            float clampedX = Mathf.Clamp(playerTransform.position.x, minX, maxX);
+            float clampedY = Mathf.Clamp(playerTransform.position.y, minY, maxY);
+            Vector2 targetPosition = new Vector2(clampedX, clampedY) + offset;
+            Vector2 smoothedPosition = Vector2.Lerp(transform.position, targetPosition, followSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
