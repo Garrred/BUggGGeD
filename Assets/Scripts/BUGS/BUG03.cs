3cdbb7a5f7aa614b9c01df212c3a6cd7ed7f3ec8
Garrred <fchen222@wisc.edu>
2021-11-05 21:40:26 -0500

    Updated player position after getting BUG03.

--- Assets/Scripts/BUGS/BUG03.cs
+++ Assets/Scripts/BUGS/BUG03.cs
@@ -5,17 +5,6 @@ using UnityEngine;
 public class BUG03 : MonoBehaviour
 {
     public ParticleSystem ps;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -24,9 +13,10 @@ public class BUG03 : MonoBehaviour
             other.gameObject.tag = "FakePlayer";
             other.gameObject.GetComponent<Player>().enabled = false;
             other.gameObject.GetComponent<Weapon>().enabled = false;
-            Instantiate(ps, transform.position - new Vector3(-5f, -5f, 0f), Quaternion.identity);
+            transform.Translate(new Vector3(0, 20f, 0));
+            Instantiate(ps, transform.position, Quaternion.identity);
             GameObject.FindGameObjectWithTag("Camera").GetComponent<Basics.CameraFollow>().ResetFollow();
-            
+            Destroy(gameObject);
         }
     }
 }
