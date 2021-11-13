627ebece68e473c6a7f3cbbdebd6659b150fd511
SCP-11 <toshi.scp11@gmail.com>
2021-11-12 18:44:43 -0600

    BUG8

---
+++ Assets/Scripts/BUGS/BUG08.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BUGS
+{
+
+    public class BUG08 : BUGFrame
+    {
+        public float movementSpeed = 10;
+        public Basics.Player player;
+        public GameObject cam;
+        private Vector2 movementInput;
+        private Rigidbody2D rb;
+        void Start()
+        {
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<Basics.Player>();
+            player.enableMovement = false;
+            BUGNumber = "08";
+            lastingTime = 5f;
+            rb = this.GetComponent<Rigidbody2D>();
+            
+
+        }
+
+        private void FixedUpdate()
+        {
+           movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            //this.transform.Translate(movementInput * movementSpeed * Time.deltaTime);
+            //rb.AddForce(movementInput * movementSpeed * Time.deltaTime);
+            rb.MovePosition(rb.position + movementInput * movementSpeed * Time.fixedDeltaTime);
+
+        }
+
+        // TODO: Delete the method in Player's script
+        public override void BugStart(){
+            cam.GetComponent<Basics.CameraFollow>().isSleeping = true;
+        }
+
+        public override void BugEnd(){}
+    }
+}
--- Assets/Scripts/Basics/CameraFollow.cs
+++ Assets/Scripts/Basics/CameraFollow.cs
@@ -28,6 +28,10 @@ namespace Basics
                 Vector2 targetPosition = new Vector2(clampedX, clampedY) + offset;
                 smoothedPosition = Vector2.Lerp(transform.position, targetPosition, followSpeed * Time.fixedDeltaTime);
             }
+            else
+            {
+                smoothedPosition = playerTransform.position;
+            }
         }
         private void LateUpdate()
         {
